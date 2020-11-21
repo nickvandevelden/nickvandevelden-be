ab9ede34d3f14914a4eb54152a2c312ad9c6eb3e
Nick Van de Velden <nickvandevelden@hotmail.comgit config --global user.email nickvandevelden@hotmail.com>
2020-11-21 10:22:30 +0100

    Added component when user is on mobile landscape

---
+++ src/pages/Landscape.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  .landscape-content {
+    min-height: 100vh;
+    position: relative;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: #222;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  h1 {
+    display: block;
+    color: #fff;
+    font-size: 9vh;
+    line-height: 1;
+    font-weight: 900;
+    text-transform: uppercase;
+    padding: 15px 25px;
+  }
+`;
+
+export const Landscape = (props) => {
+  return (
+    <Styles>
+      <div className='landscape-content'>
+        <h1>Please rotate your device for the best experience.</h1>
+      </div>
+    </Styles>
+  );
+};
