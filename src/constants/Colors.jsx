878cd624042d430125caba6a6d4658311cdefaef
Nick Van de Velden <nickvandevelden@hotmail.comgit config --global user.email nickvandevelden@hotmail.com>
2020-11-21 20:36:29 +0100

    Added contact page

---
+++ src/constants/Colors.jsx
@@ -0,0 +1,6 @@
+export const COLORS = {
+  primary: '#222',
+  secondary: '#00d6a8',
+  tertiary: '#fff',
+  text: '#000',
+};
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  GrFacebook as FacebookIcon,
+  GrInstagram as InstagramIcon,
+  GrGithub as GithubIcon,
+} from 'react-icons/gr';
+import { BsEnvelope as EmailIcon } from 'react-icons/bs';
+import { isMobileOnly } from 'react-device-detect';
+import { COLORS } from './../constants/Colors';
+
+const Styles = styled.div`
+  .contact-content {
+    min-height: 50vh;
+    position: relative;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: ${COLORS.secondary};
+  }
+
+  .icons {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: ${isMobileOnly ? 'column' : ''};
+    font-size: ${isMobileOnly ? '10vw' : '5vw'};
+  }
+
+  .icon {
+    margin-left: ${isMobileOnly ? '' : '4vw'};
+    margin-right: ${isMobileOnly ? '' : '4vw'};
+    margin-top: ${isMobileOnly ? '2.5vh' : ''};
+    margin-bottom: ${isMobileOnly ? '2.5vh' : ''};
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: ${COLORS.text};
+    transition: 0.3s;
+    &:hover {
+      color: ${COLORS.tertiary};
+    }
+  }
+`;
+
+export const Contact = (props) => {
+  return (
+    <Styles>
+      <div className='contact-content'>
+        <div className='icons'>
+          <div className='icon facebook'>
+            <a href='https://www.facebook.com/nick.vandevelden.3/' target='_blank' rel='noreferrer'>
+              <FacebookIcon />
+            </a>
+          </div>
+          <div className='icon instagram'>
+            <a href='https://www.instagram.com/nickvandevelden/' target='_blank' rel='noreferrer'>
+              <InstagramIcon />
+            </a>
+          </div>
+          <div className='icon github'>
+            <a href='https://github.com/nickvandevelden/' target='_blank' rel='noreferrer'>
+              <GithubIcon />
+            </a>
+          </div>
+          <div className='icon email'>
+            <a href='mailto: nickvandevelden@hotmail.com'>
+              <EmailIcon />
+            </a>
+          </div>
+        </div>
+      </div>
+    </Styles>
+  );
+};
