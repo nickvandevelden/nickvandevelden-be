d1cffdfc1635453c2dac09c6c0bc2dae341ccef5
Nick Van de Velden <nickvandevelden@hotmail.comgit config --global user.email nickvandevelden@hotmail.com>
2020-11-22 11:09:56 +0100

    Added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { isMobileOnly } from 'react-device-detect';
+import { COLORS } from '../constants/Colors';
+
+const Styles = styled.div`
+  .footer-content {
+    min-height: ${isMobileOnly ? '12.5vh' : '25vh'};
+    position: relative;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: ${COLORS.primary};
+    color: ${COLORS.tertiary};
+    font-size: ${isMobileOnly ? '4vw' : '1.5vw'};
+  }
+  .copyright {
+    padding-bottom: 2%;
+  }
+
+  .made-with {
+    font-size: ${isMobileOnly ? '3vw' : '1vw'};
+  }
+`;
+
+export const Footer = (props) => {
+  return (
+    <Styles>
+      <div className='footer-content'>
+        <div className='copyright'>© 2020 - Nick Van de Velden - All Rights Reserved</div>
+        <div className='made-with'>Made from scratch with ReactJS</div>
+      </div>
+    </Styles>
+  );
+};
